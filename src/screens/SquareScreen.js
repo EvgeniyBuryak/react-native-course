24e102ea504e7ea78fdaad4f0a6b35f2f856618a
Jhonah <buryakone@gmail.com>
2021-08-12 13:22:59 +0700

    added colorCounter component for SquareScreen

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import ColorCounter from '../components/ColorCounter';
 
 const SquareScreen = () => {
     return (
         <View>
             <Text>Square Screen</Text>
+            <ColorCounter color="Red" />
+            <ColorCounter color="Blue" />
+            <ColorCounter color="Green" />
         </View>
     );
 };
