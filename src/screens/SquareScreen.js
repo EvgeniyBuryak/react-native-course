b282070d562c431653617979153abedc9adc5237
Jhonah <buryakone@gmail.com>
2021-08-23 14:56:27 +0700

    переименовал в reducer переменные согласно соглашению разработчиков об правильном именнование

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,26 +6,26 @@ const COLOR_INCREMENT = 15;
 
 const reducer = (state, action) => {
     // state === { red: number, green: number, blue: number }
-    // action === { colotToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
+    // action === { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15 }
 
-    switch (action.colorToChange) {
-        case 'red':
+    switch (action.type) {
+        case 'change_red':
             // never going to do:
             // state.red = state.red - 15;
             // ...state === {red: 0, green: 0, blue: 0}
-            // similar below { green: 0, blue: 0, red: state.red + action.amount } 
-            return state.red + action.amount > 255 || state.red + action.amount < 0
+            // similar below { green: 0, blue: 0, red: state.red + action.payload } 
+            return state.red + action.payload > 255 || state.red + action.payload < 0
               ? state
-              : { ...state, red: state.red + action.amount };
-        case 'green':
-            // { red: 0, blue: 0, green: state.green + action.amount }
-            return state.green + action.amount > 255 || state.green + action.amount < 0
+                : { ...state, red: state.red + action.payload };
+        case 'change_green':
+            // { red: 0, blue: 0, green: state.green + action.payload }
+            return state.green + action.payload > 255 || state.green + action.payload < 0
                 ? state
-                : { ...state, green: state.green + action.amount };
-        case 'blue':
-            return state.blue + action.amount > 255 || state.blue + action.amount < 0
+                : { ...state, green: state.green + action.payload };
+        case 'change_blue':
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0
                 ? state
-                : { ...state, blue: state.blue + action.amount };
+                : { ...state, blue: state.blue + action.payload };
         default:
             return state;
     }
@@ -41,18 +41,18 @@ const SquareScreen = () => {
         <View>
             <Text>Square Screen</Text>
             <ColorCounter
-                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })}
-                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })}
+                onIncrease={() => dispatch({ type: 'change_red', payload: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ type: 'change_red', payload: -1 * COLOR_INCREMENT })}
                 color="Red"
             />
             <ColorCounter
-                onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
-                onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })}
+                onIncrease={() => dispatch({ type: 'change_blue', payload: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ type: 'change_blue', payload: -1 * COLOR_INCREMENT })}
                 color="Blue"
             />
             <ColorCounter
-                onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })}
-                onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })}
+                onIncrease={() => dispatch({ type: 'change_green', payload: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ type: 'change_green', payload: -1 * COLOR_INCREMENT })}
                 color="Green"
             />
             <View
